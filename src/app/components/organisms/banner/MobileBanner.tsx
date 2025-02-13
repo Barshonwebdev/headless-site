e9212f802d52bc4370f111c405a399e59e67c1bc
Barshon <barshonweb@gmail.com>
2025-02-13 15:46:42 +0600

    banner bg and text responsive

--- src/app/components/organisms/banner/MobileBanner.tsx
+++ src/app/components/organisms/banner/MobileBanner.tsx
@@ -7,10 +7,10 @@ import bg from '../../../../../public/images/mobilebanner.svg'
 const MobileBanner = () => {
   return (
     <div className="flex space-y-8 items-center bg-cover h-[484px]  flex-col justify-center" style={{ backgroundImage: `url(${bg.src})` }} >
-      <Text child="Emotions through images." className="text-5xl text-center text-white font-extrabold"></Text>
+      <Text child="Emotions through images." className="text-3xl text-center text-white font-extrabold"></Text>
       <Text
         child="Explore our curated collection of premium digital art to inspire your next creative masterpiece."
-        className="text-xl text-white text-center font-normal"
+        className="text-lg text-white text-opacity-85 text-center "
       ></Text>
       <Searchbox></Searchbox>
       <SearchSuggest></SearchSuggest>
