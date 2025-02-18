90b038abea654686dfac4cb72796b2e7d49da467
Barshon <barshonweb@gmail.com>
2025-02-18 16:49:52 +0600

    smart section complete (mobile responsive)

--- src/app/components/organisms/smart/Smart.tsx
+++ src/app/components/organisms/smart/Smart.tsx
@@ -11,19 +11,19 @@ const Smart = () => {
     <div className="py-20 ">
       <Text
         child="Smart Art Suggestions  to Elevate Your Creative Projects"
-        className="px-[440px] mb-10 text-4xl font-bold text-center"
+        className="lg:px-[440px] px-4 mb-10 lg:text-4xl text-3xl font-bold text-center"
       ></Text>
-      <div className="flex items-center px-64 ">
-        <div className="flex w-1/2 ">
+      <div className="flex flex-col lg:flex-row items-center  lg:px-64 ">
+        <div className="flex order-2 lg:order-1 lg:w-1/2 mt-8 lg:mt-0 ">
           <ImageComponent
             source={smartimg}
-            className=""
+            className="lg:px-0 px-2"
           ></ImageComponent>
         </div>
-        <div className="space-y-8 px-20 w-1/2 ">
-            <SmartParts titletext="Personalized Art Suggestion" titleClass="text-sm font-bold mb-3 " descriptiontext="Receive handpicked digital art suggestions based on your preferences and past downloads." descriptionClass="text-xs text-gray-600 font-normal" imgclass="w-10 mb-4" smartimg={smartimg1}></SmartParts>
-            <SmartParts titletext="High Quality & Premium Stock Images" titleClass="text-sm font-bold mb-3 " descriptiontext="Explore a diverse library of stunning premium stock images for all your creative endeavors." descriptionClass="text-xs text-gray-600 font-normal" imgclass="w-10 mb-4" smartimg={smartimg2}></SmartParts>
-            <SmartParts titletext="Explore Trending Styles" titleClass="text-sm font-bold mb-3" descriptiontext="Stay ahead of the curve with trending digital art styles and techniques in-demand artistic trends to elevate your creations" descriptionClass="text-xs text-gray-600 font-normal" imgclass="w-10 mb-4" smartimg={smartimg3}></SmartParts>
+        <div className="space-y-8 lg:px-20 lg:w-1/2 order-1 lg:order-2 px-14 ">
+            <SmartParts titletext="Personalized Art Suggestion" titleClass="text-sm font-bold mb-3 text-center lg:text-start " descriptiontext="Receive handpicked digital art suggestions based on your preferences and past downloads." descriptionClass="lg:text-start text-center text-xs text-gray-600 font-normal" imgclass="w-10 mb-4 mx-auto lg:mx-0" smartimg={smartimg1}></SmartParts>
+            <SmartParts titletext="High Quality & Premium Stock Images" titleClass="lg:text-start text-center text-sm font-bold mb-3 " descriptiontext="Explore a diverse library of stunning premium stock images for all your creative endeavors." descriptionClass="lg:text-start text-center text-xs text-gray-600 font-normal" imgclass="w-10 mb-4 mx-auto lg:mx-0" smartimg={smartimg2}></SmartParts>
+            <SmartParts titletext="Explore Trending Styles" titleClass="lg:text-start text-center text-sm font-bold mb-3" descriptiontext="Stay ahead of the curve with trending digital art styles and techniques in-demand artistic trends to elevate your creations" descriptionClass="lg:text-start text-center text-xs text-gray-600 font-normal" imgclass="mx-auto w-10 mb-4 lg:mx-0" smartimg={smartimg3}></SmartParts>
             
         </div>
       </div>
