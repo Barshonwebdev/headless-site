9d0c8c326bf1b75f2362b14a7a815e667f389a1b
Barshon <barshonweb@gmail.com>
2025-02-20 10:11:51 +0600

    download section complete (mobile responsive)

--- src/app/components/organisms/download/Download.tsx
+++ src/app/components/organisms/download/Download.tsx
@@ -7,19 +7,19 @@ import mobileimg from '../../../../../public/images/Iphone 12 Max.svg'
 import pointingimg from '../../../../../public/images/pointing.svg'
 const Download = () => {
     return (
-        <div className='bg-[#E7E5FF] my-20 h-[400px] items-center flex  px-[200px] py-[200px] space-x-24 '>
+        <div className='bg-[#E7E5FF] md:my-20 my-10 md:h-[400px] h-[370px] items-center flex px-4 md:px-[200px] md:py-[200px]  space-x-24 '>
             <div className='space-y-4 items-center  '>
-                <Text child='Download Our App and Have Access to Digital Art On The Go' className='font-bold text-4xl'></Text>
+                <Text child='Download Our App and Have Access to Digital Art On The Go' className='font-bold text-3xl md:text-4xl'></Text>
                 <Text child='Carry our digital art library in your pocket with the ImageQuest mobile app - Discover, Download, Create.' className='text-base text-[#3C3C3C]'></Text>
                 <div className='flex space-x-6'>
                     <button><ImageComponent className='w-[160px] ' source={playstoreimg}></ImageComponent></button>  
                     <button><ImageComponent className='w-[150px] ' source={applestoreimg}></ImageComponent></button>  
-                    <ImageComponent className='w-2/3 top-11 right-6 relative' source={pointingimg}></ImageComponent>
+                    <ImageComponent className='w-2/3 top-11 right-32 md:right-6 relative ' source={pointingimg}></ImageComponent>
             </div>
                 </div>
                 
 
-            <div className='w-fit  '>
+            <div className='w-fit hidden md:block '>
                 <ImageComponent className='' source={mobileimg}></ImageComponent>
             </div>
         </div>
