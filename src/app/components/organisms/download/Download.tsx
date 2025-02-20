7291f96a7408085e752d9e8434a81bd255cba630
Barshon <barshonweb@gmail.com>
2025-02-20 10:00:29 +0600

    download section complete (desktop only)

--- src/app/components/organisms/download/Download.tsx
+++ src/app/components/organisms/download/Download.tsx
@@ -7,8 +7,8 @@ import mobileimg from '../../../../../public/images/Iphone 12 Max.svg'
 import pointingimg from '../../../../../public/images/pointing.svg'
 const Download = () => {
     return (
-        <div className='bg-[#E7E5FF] my-20 h-[400px] items-center flex  px-[200px] py-[100px] space-x-24 '>
-            <div className='space-y-6 items-center '>
+        <div className='bg-[#E7E5FF] my-20 h-[400px] items-center flex  px-[200px] py-[200px] space-x-24 '>
+            <div className='space-y-4 items-center  '>
                 <Text child='Download Our App and Have Access to Digital Art On The Go' className='font-bold text-4xl'></Text>
                 <Text child='Carry our digital art library in your pocket with the ImageQuest mobile app - Discover, Download, Create.' className='text-base text-[#3C3C3C]'></Text>
                 <div className='flex space-x-6'>
