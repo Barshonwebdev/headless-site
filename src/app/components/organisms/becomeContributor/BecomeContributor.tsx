1f4e3b8f083668737316774b1c67dad24333e77f
Barshon <barshonweb@gmail.com>
2025-02-25 14:54:36 +0600

    insight section with cards links (desktop + mobile)

--- src/app/components/organisms/becomeContributor/BecomeContributor.tsx
+++ src/app/components/organisms/becomeContributor/BecomeContributor.tsx
@@ -14,7 +14,7 @@ const BecomeContributor = () => {
             <Text child="Help us create the world's largest stock photo library, become a contributor and get paid for your assets." className='lg:px-80 px-4 text-center text-[#3C3C3C]'></Text>
             </div>
             <AllPurposeButton
-            className="font-medium flex mx-auto bg-black  text-white border-black border-2 px-10 py-2 mt-10 rounded"
+            className="font-medium flex relative bg-black  text-white border-black border-2 px-10 py-2 mt-10 rounded"
             child="Let's contribute"
           ></AllPurposeButton>
           
--- src/app/components/organisms/insights/Insight.tsx
+++ src/app/components/organisms/insights/Insight.tsx
@@ -5,15 +5,15 @@ import insight1 from '../../../../../public/images/insightimg1.svg'
 import insight2 from '../../../../../public/images/insightimg2.svg'
 import insight3 from '../../../../../public/images/insightimg3.svg'
 import AllPurposeButton from '../../atoms/buttons/AllPurposeButton';
+import Link from 'next/link';
 const Insight = () => {
     return (
         <div className='lg:mt-32 my-20 space-y-10'>
             <Text child='Inspirations & Insights Of The Digital Art' className='text-center lg:text-4xl text-2xl font-extrabold'></Text>
             <div className='flex gap-7 flex-col lg:flex-row '>
-                <Card className='border border-gray-200 shadow-lg border-t-0 px-4' imgclassName='' imageSource={insight1} category='Inspiration'  duration='4 min read' title='Paige Deasley`s Photos Capture the Joy of Outdoor Adventures' categoryClass='text-xs font-bold mt-4 mb-6' titleClass='font-bold mb-2' durationClass='text-xs text-gray-500 mb-4' ></Card>
-                <Card className='border border-gray-200 shadow-lg border-t-0 px-4' imgclassName='' imageSource={insight2} category='Trends'  duration='4 min read' title='Paige Deasley`s Photos Capture the Joy of Outdoor Adventures' categoryClass='text-xs font-bold mt-4 mb-6' titleClass='font-bold mb-2' durationClass='text-xs text-gray-500 mb-4' ></Card>
-                <Card className='border border-gray-200 shadow-lg border-t-0 px-4' imgclassName='' imageSource={insight3} category='Tips & tricks'  duration='4 min read' title='Paige Deasley`s Photos Capture the Joy of Outdoor Adventures' categoryClass='text-xs font-bold mt-4 mb-6' titleClass='font-bold mb-2' durationClass='text-xs text-gray-500 mb-4' ></Card>
-                
+                <Link href=''><Card className='border border-gray-200 shadow-lg border-t-0 px-4' imgclassName='' imageSource={insight1} category='Inspiration'  duration='4 min read' title='Paige Deasley`s Photos Capture the Joy of Outdoor Adventures' categoryClass='text-xs font-bold mt-4 mb-6' titleClass='font-bold mb-2' durationClass='text-xs text-gray-500 mb-4' ></Card></Link>
+                <Link href=''><Card className='border border-gray-200 shadow-lg border-t-0 px-4' imgclassName='' imageSource={insight2} category='Trends'  duration='4 min read' title='Paige Deasley`s Photos Capture the Joy of Outdoor Adventures' categoryClass='text-xs font-bold mt-4 mb-6' titleClass='font-bold mb-2' durationClass='text-xs text-gray-500 mb-4' ></Card></Link>
+                <Link href=''><Card className='border border-gray-200 shadow-lg border-t-0 px-4' imgclassName='' imageSource={insight3} category='Tips & tricks'  duration='4 min read' title='Paige Deasley`s Photos Capture the Joy of Outdoor Adventures' categoryClass='text-xs font-bold mt-4 mb-6' titleClass='font-bold mb-2' durationClass='text-xs text-gray-500 mb-4' ></Card></Link>
             </div> 
             <div className='flex justify-center'>
             <AllPurposeButton
