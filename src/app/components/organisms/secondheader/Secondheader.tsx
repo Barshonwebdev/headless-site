dffe44066e8d274aab729790e76a5f65173ad1b2
Barshon <barshonweb@gmail.com>
2025-02-25 12:19:16 +0600

    desktop second header navlinks and icons

--- src/app/components/organisms/secondheader/Secondheader.tsx
+++ src/app/components/organisms/secondheader/Secondheader.tsx
@@ -7,42 +7,43 @@ import bookmark from "../../../../../public/icons/bookmark.svg";
 import cart from "../../../../../public/icons/cart.svg";
 import glitter from '../../../../../public/icons/glitter.svg'
 import NavigateDropdown from '../../molecules/NavigationDropdown';
+import Link from 'next/link';
 const Secondheader = () => {
     return (
       <div className="bg-black">
-        <div className=" ">
+        <div className=" "> 
           <Width>
             {" "}
             <div className="flex  items-center justify-between py-4">
               <div className="flex text-white items-center space-x-8">
                 <NavigateDropdown ></NavigateDropdown>
-                <Text className=" " child="VIDEOS"></Text>
-                <div className="flex items-center space-x-1">
-                <IconComponent source={glitter} className=""
+                <Link href={''}><Text className=" " child="VIDEOS"></Text></Link>
+                
+                <Link href={''} className='flex items-center space-x-1'><IconComponent source={glitter} className=""
                   width={20}
                   height={20} ></IconComponent>
-                <Text className=" " child="AI GENERATED"></Text>
-                </div>
-                <Text className=" " child="BROWSE CONTENT"></Text>
+                <Text className=" " child="AI GENERATED"></Text></Link>
+                
+                <Link href={''}><Text className=" " child="BROWSE CONTENT"></Text></Link>
               </div>
 
-              <div className="flex space-x-9">
+              <div className="flex space-x-9 items-center">
                 <AllPurposeButton
                   className="font-medium bg-black text-white border-white border-2 rounded-3xl px-4 py-2"
                   child="Get Involved"
                 ></AllPurposeButton>
-                <IconComponent
+                <Link href={''} ><IconComponent
                   className=""
                   width={30}
                   height={30}
                   source={bookmark}
-                ></IconComponent>
-                <IconComponent
+                ></IconComponent></Link>
+                <Link href={''} ><IconComponent
                   className=""
-                  width={30}
+                  width={25}
                   height={30}
                   source={cart}
-                ></IconComponent>
+                ></IconComponent></Link>
               </div>
             </div>
           </Width>
