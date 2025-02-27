f3559ab9e4aba756307843c86c421cf69c5c417d
Barshon <barshonweb@gmail.com>
2025-02-27 14:50:33 +0600

    insight section responsive fix

--- src/app/components/organisms/insights/Insight.tsx
+++ src/app/components/organisms/insights/Insight.tsx
@@ -8,16 +8,16 @@ import AllPurposeButton from "../../atoms/buttons/AllPurposeButton";
 import Link from "next/link";
 const Insight = () => {
   return (
-    <div className="lg:mt-32 my-20 space-y-10">
+    <div className="lg:mt-64 my-20 space-y-10">
       <Text
         child="Inspirations & Insights Of The Digital Art"
         className="text-center lg:text-4xl text-2xl font-extrabold"
       ></Text>
-      <div className="flex gap-7 flex-col lg:flex-row ">
+      <div className="flex justify-center gap-7 flex-col lg:flex-row ">
         <Link href="">
           <Card
             className="border border-gray-200 shadow-lg border-t-0 px-4"
-            imgclassName=""
+            imgclassName="w-full"
             imageSource={insight1}
             category="Inspiration"
             duration="4 min read"
@@ -30,7 +30,7 @@ const Insight = () => {
         <Link href="">
           <Card
             className="border border-gray-200 shadow-lg border-t-0 px-4"
-            imgclassName=""
+            imgclassName="w-full"
             imageSource={insight2}
             category="Trends"
             duration="4 min read"
@@ -43,7 +43,7 @@ const Insight = () => {
         <Link href="">
           <Card
             className="border border-gray-200 shadow-lg border-t-0 px-4"
-            imgclassName=""
+            imgclassName="w-full"
             imageSource={insight3}
             category="Tips & tricks"
             duration="4 min read"
