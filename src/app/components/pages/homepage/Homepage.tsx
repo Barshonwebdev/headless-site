ab1695422995baeaf21990eb3c360c1b452897c1
Barshon <barshonweb@gmail.com>
2025-03-02 13:22:00 +0600

    homepage components organizing

--- src/app/components/pages/homepage/Homepage.tsx
+++ src/app/components/pages/homepage/Homepage.tsx
@@ -1,62 +1,10 @@
 import React from "react";
-import Header from "../../organisms/common/Header";
-import Width from "../../layout/Width";
-import Secondheader from "../../organisms/secondheader/Secondheader";
-import Banner from "../../organisms/banner/Banner";
-import Trending from "../../organisms/trending/Trending";
-import Desktop from "../../layout/Desktop";
-import Mobile from "../../layout/Mobile";
-import MobileHeader from "../../organisms/MobileHeader/MobileHeader";
-import MobileBanner from "../../organisms/banner/MobileBanner";
-import TrendingMobile from "../../organisms/trending/TrendingMobile";
-import Explore from "../../organisms/Explore/Explore";
-import ExploreMobile from "../../organisms/Explore/ExploreMobile";
-import Smart from "../../organisms/smart/Smart";
-import Inspiration from "../../organisms/inspiration/Inspiration";
-import Weekly from "../../organisms/weekly/Weekly";
-import Download from "../../organisms/download/Download";
-import Insight from "../../organisms/insights/Insight";
-import BecomeContributor from "../../organisms/becomeContributor/BecomeContributor";
-import Footer from "../../organisms/common/Footer";
+import PageTemplate from "../../template/PageTemplate";
 
 const Homepage = () => {
   return (
     <div>
-
-      {/* desktop exclusive view */}
-      <Desktop>
-        <Width>
-          <Header></Header>
-        </Width>
-        <Secondheader></Secondheader>
-        <Banner></Banner>
-        <Width>
-          <Trending></Trending>
-        </Width>
-        <Explore></Explore>
-      </Desktop>
-
-      {/* mobile exclusive view  */}
-      <Mobile>
-        <MobileHeader></MobileHeader>
-        <MobileBanner></MobileBanner>
-        <TrendingMobile></TrendingMobile>
-        <ExploreMobile></ExploreMobile>
-      </Mobile>
-      
-
-      {/* common for all views  */}
-      <Width>
-        <Smart></Smart>
-      </Width>
-      <Inspiration></Inspiration>
-      <Weekly></Weekly>
-      <Download></Download>
-      <Width>
-        <Insight></Insight>
-      </Width>
-      <Width><BecomeContributor></BecomeContributor></Width>
-      <Footer></Footer>
+      <PageTemplate></PageTemplate>
     </div>
   );
 };
--- src/app/components/template/PageTemplate.tsx
+++ src/app/components/template/PageTemplate.tsx
@@ -1,10 +1,66 @@
+
+
+
 import React from "react";
-import Homepage from "../pages/homepage/Homepage";
+import Desktop from "../layout/Desktop";
+import Width from "../layout/Width";
+import Header from "../organisms/common/Header";
+import Secondheader from "../organisms/secondheader/Secondheader";
+import Banner from "../organisms/banner/Banner";
+import Trending from "../organisms/trending/Trending";
+import Explore from "../organisms/Explore/Explore";
+import Mobile from "../layout/Mobile";
+import MobileHeader from "../organisms/MobileHeader/MobileHeader";
+import MobileBanner from "../organisms/banner/MobileBanner";
+import TrendingMobile from "../organisms/trending/TrendingMobile";
+import ExploreMobile from "../organisms/Explore/ExploreMobile";
+import Smart from "../organisms/smart/Smart";
+import Inspiration from "../organisms/inspiration/Inspiration";
+import Weekly from "../organisms/weekly/Weekly";
+import Download from "../organisms/download/Download";
+import Insight from "../organisms/insights/Insight";
+import BecomeContributor from "../organisms/becomeContributor/BecomeContributor";
+import Footer from "../organisms/common/Footer";
+
 
 const PageTemplate = () => {
   return (
     <div>
-      <Homepage></Homepage>
+      {/* desktop exclusive view */}
+      <Desktop>
+        <Width>
+          <Header></Header>
+        </Width>
+        <Secondheader></Secondheader>
+        <Banner></Banner>
+        <Width>
+          <Trending></Trending>
+        </Width>
+        <Explore></Explore>
+      </Desktop>
+
+      {/* mobile exclusive view  */}
+      <Mobile>
+        <MobileHeader></MobileHeader>
+        <MobileBanner></MobileBanner>
+        <TrendingMobile></TrendingMobile>
+        <ExploreMobile></ExploreMobile>
+      </Mobile>
+
+      {/* common for all views  */}
+      <Width>
+        <Smart></Smart>
+      </Width>
+      <Inspiration></Inspiration>
+      <Weekly></Weekly>
+      <Download></Download>
+      <Width>
+        <Insight></Insight>
+      </Width>
+      <Width>
+        <BecomeContributor></BecomeContributor>
+      </Width>
+      <Footer></Footer>
     </div>
   );
 };
