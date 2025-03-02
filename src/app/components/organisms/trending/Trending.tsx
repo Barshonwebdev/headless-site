8005325b9c98106278659b4013d842e6f026b9e6
Barshon <barshonweb@gmail.com>
2025-03-02 11:23:13 +0600

    trending section responsive fix

--- src/app/components/organisms/trending/Trending.tsx
+++ src/app/components/organisms/trending/Trending.tsx
@@ -1,4 +1,4 @@
-import React from "react"; 
+import React from "react";
 import Text from "../../atoms/texts/Text";
 import AllPurposeButton from "../../atoms/buttons/AllPurposeButton";
 import img1 from "../../../../../public/images/trendingimage1.svg";
@@ -9,51 +9,50 @@ import ImageComponent from "../../atoms/Image/ImageComponent";
 import Link from "next/link";
 const Trending = () => {
   return (
-    <div className="my-24 flex items-center  flex-col">
+    <div className="my-24 flex   flex-col lg:max-w-[1620px] mx-auto">
       <div>
-      <div className="flex justify-between items-center ">
-        <Text
-          child="Discover Trending Categories"
-          className="text-black font-bold text-4xl text-center"
-        ></Text>
-        <AllPurposeButton
-          child="Explore all categories"
-          className="text-sm border-black border p-3 rounded font-semibold"
-        ></AllPurposeButton>
-      </div>
-      <div className="flex flex-col  items-center ">
-      
-      <div className="flex gap-7 mt-10 ">
-        <Link href="" className="group">
-          <ImageComponent source={img1} className=""></ImageComponent>
+        <div className="flex justify-between items-center ">
           <Text
-            child="Images"
-            className="text-center -mt-11 invisible transition translate-y-12 text-white group-hover:visible group-hover:translate-y-0 "
+            child="Discover Trending Categories"
+            className="text-black font-bold text-4xl text-center"
           ></Text>
-        </Link>
-        <Link href="" className="group">
-          <ImageComponent source={img2} className=""></ImageComponent>
-          <Text
-            child="AI Designs"
-            className="text-center -mt-11 invisible transition translate-y-12 text-white group-hover:visible group-hover:translate-y-0 "
-          ></Text>
-        </Link>
-        <Link href="" className="group">
-          <ImageComponent source={img3} className=""></ImageComponent>
-          <Text
-            child="Paintings"
-            className="text-center -mt-11 invisible transition translate-y-12 text-white group-hover:visible group-hover:translate-y-0 "
-          ></Text>
-        </Link>
-        <Link href="" className="group">
-          <ImageComponent source={img4} className=""></ImageComponent>
-          <Text
-            child="3D Models"
-            className="text-center -mt-11 invisible transition translate-y-12 text-white group-hover:visible group-hover:translate-y-0 "
-          ></Text>
-        </Link>
-      </div>
-      </div>
+          <AllPurposeButton
+            child="Explore all categories"
+            className="text-sm border-black border p-3 rounded font-semibold"
+          ></AllPurposeButton>
+        </div>
+        <div className="flex flex-col  ">
+          <div className="flex justify-between gap-7 mt-10 ">
+            <Link href="" className="group">
+              <ImageComponent source={img1} className=""></ImageComponent>
+              <Text
+                child="Images"
+                className="text-center -mt-11 invisible transition translate-y-12 text-white group-hover:visible group-hover:translate-y-0 "
+              ></Text>
+            </Link>
+            <Link href="" className="group">
+              <ImageComponent source={img2} className=""></ImageComponent>
+              <Text
+                child="AI Designs"
+                className="text-center -mt-11 invisible transition translate-y-12 text-white group-hover:visible group-hover:translate-y-0 "
+              ></Text>
+            </Link>
+            <Link href="" className="group">
+              <ImageComponent source={img3} className=""></ImageComponent>
+              <Text
+                child="Paintings"
+                className="text-center -mt-11 invisible transition translate-y-12 text-white group-hover:visible group-hover:translate-y-0 "
+              ></Text>
+            </Link>
+            <Link href="" className="group">
+              <ImageComponent source={img4} className=""></ImageComponent>
+              <Text
+                child="3D Models"
+                className="text-center -mt-11 invisible transition translate-y-12 text-white group-hover:visible group-hover:translate-y-0 "
+              ></Text>
+            </Link>
+          </div>
+        </div>
       </div>
     </div>
   );
